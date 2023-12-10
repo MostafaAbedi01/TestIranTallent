1524f1f256a13c185acfaa7f545a273f8780a803
DeployEniac <DeployEniac@sepasholding.com>
2023-12-10 14:54:29 +0330

    add project

---
+++ testTalent/Controllers/NodeController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testTalent.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NodeController : ControllerBase
+    {
+        private static readonly Node[] Summaries = new[]
+        {
+            new Node
+            {
+                Id = 1,
+                Title = "گوشی موبایل",
+                ParentId = null
+            },
+            new Node
+            {
+                Id = 2,
+                Title = "تبلت",
+                ParentId = null
+            }
+            ,
+            new Node
+            {
+                Id =3,
+                Title = "لوازم جانبی",
+                ParentId = null
+            }
+            ,
+            new Node
+            {
+                Id = 4,
+                Title = "هدفون",
+                ParentId = 3
+            },
+            new Node
+            {
+                Id = 5,
+                Title = "هدفون",
+                ParentId = 3
+            },
+            new Node
+            {
+            Id =6,
+            Title = "اسپیکر",
+            ParentId = null
+            }
+        };
+
+        [HttpGet(Name = "GetNodes")]
+        public IEnumerable<Node> GetNodes()
+        {
+            // Return the tree to a certain level (change the 'n' value)
+            int n = 2; // Define the level
+
+            var nodes = CreateTree(Summaries, null, n);
+            return nodes;
+        }
+        private List<Node> CreateTree(IEnumerable<Node> nodes, int? parentId, int remainingLevels)
+        {
+            var childNodes = nodes.Where(node => node.ParentId == parentId).ToList();
+
+            foreach (var node in childNodes)
+            {
+                if (remainingLevels > 1)
+                {
+                    node.Children = CreateTree(nodes, node.Id, remainingLevels - 1);
+                }
+            }
+
+            return childNodes;
+        }
+    }
+}
---
+++ testTalent/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace testTalent
+{
+    public class Node
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Image { get; set; }
+        public string? Link { get; set; }
+        public int? ParentId { get; set; }
+        public List<Node> Children { get; set; } = new List<Node>();
+    }
+
+}
